e00c8d1e0a668c3f16c87612a2f41b634cd3d6e5
Ian Calica <Iggc654@gmail.com>
2025-03-18 14:47:41 +0800

    Updated

--- utils/AxiosInstance.ts
+++ utils/AxiosInstance.ts
@@ -4,7 +4,7 @@ const schoolIpAdd = "192.168.244.111";
 const devIpAdd = "192.168.100.8";
 
 const AxiosInstance: AxiosInstanceType = axios.create({
-  baseURL: `http://${devIpAdd}:5000/api/v1`,
+  baseURL: `http://${schoolIpAdd}:5000/api/v1`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
